d73209f34e02b1fef734e153d924963b6cb381ae
SimonD311 <deleysimon@gmail.com>
2022-03-09 22:23:13 +0100

    user.js fixed thx to Owlive

--- commands/user.js
+++ commands/user.js
@@ -1,10 +1,17 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require(`@discordjs/builders`);
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName(`user`)
-		.setDescription(`Replies with user info!`),
-	async execute(interaction) {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-	},
+    data: new SlashCommandBuilder()
+        .setName(`user`)
+        .setDescription(`Replies with user info!`)
+        .addUserOption((option) =>
+            option
+                .setName(`user`)
+                .setDescription(`Who's info do you want?`)
+                .setRequired(false)
+        ),
+    async execute(interaction) {
+        const user = interaction.options.getUser(`user`);
+        await interaction.reply(`User: ${user}\nId: ${user.id}`);
+    },
 };
