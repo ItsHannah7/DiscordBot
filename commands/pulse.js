8de1277160b6af47cffa8a0723d3f79aea4e9a56
SimonD311 <deleysimon@gmail.com>
2022-03-09 22:41:29 +0100

    syntax cleanup

--- commands/pulse.js
+++ commands/pulse.js
@@ -1,12 +1,12 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageAttachment } = require('discord.js');
+const { SlashCommandBuilder } = require(`@discordjs/builders`);
+const { MessageAttachment } = require(`discord.js`);
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('pulse')
-		.setDescription('Ferret = pog'),
+		.setName(`pulse`)
+		.setDescription(`Ferret = pog`),
 	async execute(interaction) {
-        const ferret = new MessageAttachment("Ferret1.jpg");
+        const ferret = new MessageAttachment(`Ferret1.jpg`);
 		await interaction.reply({files: [ferret]});
 	},
 };
